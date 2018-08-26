09fc2156ccbebb6fd5cd2fabe750ac643f27509e
MitkoZhelev <mitko.j.zhelev@gmail.com>
2018-08-27 01:46:20 +0300

    Connected the HTML with the typescrip WITHOUT scope

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+ number1 :number;
+ number2 : number;
+ public PrintOne (first) {
+   this.number1 = first;
+ }
+ public PrintSecond(second){
+   this.number2 = second;
+ }
+ public Add () {
+   console.log(this.number2 + this.number2);
+ }
+
 }
