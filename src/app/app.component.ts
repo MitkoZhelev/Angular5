6bd1f807c2227df4947e6d6812e3d0bce7a248ca
MitkoZhelev <mitko.j.zhelev@gmail.com>
2018-08-27 21:29:35 +0300

    Added other operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,35 @@ export class AppComponent {
   title = 'my-app';
  number1 :number;
  number2 : number;
+ result : number;
  public PrintOne (first) {
    this.number1 = first;
  }
  public PrintSecond(second){
    this.number2 = second;
  }
- public Add () {
-   console.log(this.number2 + this.number2);
+ public Add () : number {
+  
+   this.result = +this.number1 +  +this.number2;
+   console.log(this.result);
+   return this.result;
+ }
+ public Substract (): number {
+  this.result = +this.number1 - +this.number2;
+  return this.result;
+ }
+ public Multiply() : number {
+   this.result = +this.number1 * +this.number2;
+   console.log(this.result);
+   return this.result;
+   
+ }
+ public Divide() : number {
+   if(this.number1 >0 && this.number2 > 0)
+   {
+     this.result = +this.number1 * +this.number2 ;
+     return this.result ;
+   }else (console.log("Cannot put negative in division");)
  }
 
 }
